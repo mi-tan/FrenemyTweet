345e1756792d2bf8587ace13ca266a287d43ff75
Nicokun <hcs.shoiyoshida@gmail.com>
2018-12-03 11:02:12 +0900

    Unityプロジェクト作成。コミットテスト。

---
+++ TwitterShooting/Assets/Scripts/Shoi/AnalysisSentence.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 分析クラス
+/// </summary>
+public class AnalysisSentence : MonoBehaviour, IAnalysis
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TwitterShooting/Assets/Scripts/Shoi/Interface/IAnalysis.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 分析インターフェース
+/// </summary>
+public interface AnalysisInterface {
+
+
+
+}

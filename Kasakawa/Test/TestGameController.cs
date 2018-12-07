adaef3742361e20df24259f89c0b4c999550fa1e
nuno <nenowawon@gmail.com>
2018-12-07 12:30:06 +0900

    UIを別シーンで管理する基盤を作成。

---
+++ Kasakawa/Test/TestGameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+using System;
+
+namespace Test
+{
+    public class TestGameController : MonoBehaviour
+    {
+        [Inject]
+        [SerializeField]
+        private TestPlayer testPlayer;
+
+        public TestPlayer TestPlayer
+        {
+            get
+            {
+                return testPlayer;
+            }
+        }
+
+        // イベントを登録するインスタンス
+        private Subject<Unit> startSubject = new Subject<Unit>();
+
+        // サブジェクトを公開する
+        public IObservable<Unit> OnGameStart
+        {
+            get { return startSubject; }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            //yield return new WaitForSeconds(1f);
+            // ゲーム開始時のイベントを実行
+            startSubject.OnNext(Unit.Default);
+        }
+    }
+
+}
---
+++ Kasakawa/Test/TestPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class TestPlayer : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float moveSpeed = 10f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 input = new Vector3();
+            input.x = Input.GetAxis("Horizontal");
+            input.y = Input.GetAxis("Vertical");
+
+            input *= Time.deltaTime * moveSpeed;
+
+            input += transform.position;
+
+            transform.position = input;
+        }
+    }
+}
+

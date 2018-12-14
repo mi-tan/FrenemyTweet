7066ce05398c476f498401497618e139ced7e39f
nuno <nenowawon@gmail.com>
2018-12-14 11:58:29 +0900

    笠川クラス再追加。

---
+++ Kasakawa/Test/TestGameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace Test
+{
+    public class TestGameController : MonoBehaviour
+    {
+        [Inject]
+        [SerializeField]
+        private TestPlayer testPlayer;
+
+        public TestPlayer TestPlayer
+        {
+            get
+            {
+                return testPlayer;
+            }
+        }
+
+        // イベントを登録するインスタンス
+        private Subject<Unit> startSubject = new Subject<Unit>();
+
+        // サブジェクトを公開する
+        public IObservable<Unit> OnGameStart
+        {
+            get { return startSubject; }
+        }
+
+        [SerializeField]
+        [Header("サブシーン名のリスト")]
+        private string[] subSceneNames;
+
+        public float TimeCount { get; private set; } = 0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            // ゲーム開始時のイベントを実行
+            startSubject.OnNext(Unit.Default);
+
+            foreach (var sceneName in subSceneNames)
+            {
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            }
+
+            // 時間をカウントする
+            (this).UpdateAsObservable()
+            .Subscribe(_ => { TimeCount += Time.deltaTime; });
+        }
+    }
+
+}
---
+++ Kasakawa/Test/TestUIController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+using UnityEngine.UI;
+
+namespace Test
+{
+    public class TestUIController : MonoBehaviour
+    {
+        [Inject]
+        [SerializeField]
+        TestGameController testGameController;
+
+        [SerializeField]
+        private GameObject startText;
+
+        [SerializeField]
+        private Text positionText;
+
+        [SerializeField]
+        private float startTextTime = 1f;
+
+        // Use this for initialization
+        void Awake()
+        {
+            startText.SetActive(false);
+            // ゲーム開始時に開始時のテキストを登録する
+            testGameController.OnGameStart.Subscribe(_ => { ActiveStartText(); })
+                .AddTo(gameObject);
+
+            // 一定時間後に開始時テキストを非表示にする
+            Observable.Timer(System.TimeSpan.FromSeconds(startTextTime))
+                .Subscribe(_ => DisableStartText())
+                .AddTo(gameObject);
+            
+            ShowPlayerPosition(Vector3.zero);
+
+            // プレイヤーの座標が変化した場合、UIを更新する
+            testGameController.ObserveEveryValueChanged(_ => testGameController.TestPlayer.transform.position)
+                .Subscribe(pos => ShowPlayerPosition(pos));
+        }
+
+        private void ActiveStartText()
+        {
+            startText.SetActive(true);
+        }
+
+        private void DisableStartText()
+        {
+            startText.SetActive(false);
+        }
+
+        private void ShowPlayerPosition(Vector3 position)
+        {
+            positionText.text = $"PlayerPos : \nx = {position.x}\ny = {position.y}\nz = {position.z}";
+        }
+
+    }
+
+}

99e70e3a893f5f381bfeb48738e9483dbf78cf7e
Mi-tan <koppepantanishi@gmail.com>
2018-12-18 09:48:28 +0900

    アイコン取得途中経過

---
+++ mi-tan/IconGet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconGet : MonoBehaviour {
+
+    [SerializeField]
+    ImageDownloder _imageDownLoder;
+
+    Twitter.AccessTokenResponse TwitterIcon;
+    string TwitterID = "";
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+    public void TwitterIconGet(Twitter.AccessTokenResponse response)
+    {
+
+        TwitterID = response.UserId;
+        Debug.Log(TwitterID);
+        _imageDownLoder.GetComponent<ImageDownloder>();
+        _imageDownLoder.Icon("");
+    }
+
+    public void OnClic()
+    {
+        Debug.Log(TwitterID);
+    }
+
+}
---
+++ mi-tan/ImageDownloder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageDownloder : MonoBehaviour
+{
+
+
+    public IEnumerator Icon(string icon_test)
+    {
+        const string url = "";
+        // wwwクラスのコンストラクタに画像URLを指定
+        WWW www = new WWW(url);
+        icon_test = www.ToString();
+
+        // 画像ダウンロード完了を待機
+        yield return icon_test;
+
+        // webサーバから取得した画像をRaw Imaugで表示する
+        RawImage rawImage = GetComponent<RawImage>();
+        rawImage.texture = www.textureNonReadable;
+
+        //ピクセルサイズ等倍に
+        rawImage.SetNativeSize();
+    }
+
+
+}

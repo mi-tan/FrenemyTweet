e2703a626bd83131023cd16b64d8cfb3504faef1
nuno <nenowawon@gmail.com>
2018-12-18 10:29:27 +0900

    プレイヤーUI管理クラス作成。

---
+++ Kasakawa/Controllers/PlayerUIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+using UnityEngine.UI;
+
+/// <summary>
+/// プレイヤー関連のUIを管理するクラス
+/// </summary>
+public class PlayerUIManager : MonoBehaviour
+{
+    [Inject]
+    [SerializeField]
+    MainGameManager gameManager;
+
+    [SerializeField]
+    private Slider hpSlider;
+
+    void Awake()
+    {
+
+
+
+    }
+
+    private void Start()
+    {
+        InitializePlayerUI();
+    }
+
+    private void InitializePlayerUI()
+    {
+        
+    }
+
+}

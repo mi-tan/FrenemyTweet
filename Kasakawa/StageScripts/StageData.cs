10726b140dee39c46297e9aa13471e05e9b9efcf
nuno <nenowawon@gmail.com>
2018-12-20 14:55:57 +0900

    ステージ管理クラス作成。Testフォルダ名前変更。

---
+++ Kasakawa/StageScripts/StageData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Stage")]
+public class StageData : ScriptableObject {
+
+    [SerializeField]
+    private string stageName;
+
+    [SerializeField]
+    private GameObject gameObject;
+}

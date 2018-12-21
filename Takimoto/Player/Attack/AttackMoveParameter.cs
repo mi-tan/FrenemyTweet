46a02a0697efa9c75a17c1c21d7537cf2d3c9301
takimoto0501 <takimoto0501@gmail.com>
2018-12-21 11:39:21 +0900

    剣通常攻撃の当たり判定の有効無効化、攻撃移動パラメータをScriptableObjectで実装

---
+++ Takimoto/Player/Attack/AttackMoveParameter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "ScriptableObject/Attack/Move")]
+public class AttackMoveParameter : ScriptableObject
+{
+    [Header("移動時間")]
+    [SerializeField]
+    private float moveTime;
+    /// <summary>
+    /// 移動時間
+    /// </summary>
+    public float MoveTime
+    {
+        get
+        {
+            return moveTime;
+        }
+    }
+
+    [Header("移動距離")]
+    [SerializeField]
+    private float moveDistance;
+    /// <summary>
+    /// 移動距離
+    /// </summary>
+    public float MoveDistance
+    {
+        get
+        {
+            return moveDistance;
+        }
+    }
+
+    [Header("移動速度")]
+    [SerializeField]
+    private float moveSpeed;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    public float MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        }
+    }
+}

9da6de210894373931f25b905d0b63fa5663d8c9
nuno <nenowawon@gmail.com>
2019-01-17 11:57:11 +0900

    ツイッターシーンの管理クラス作成。

---
+++ Kasakawa/SceneBases/TwitterSceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterSceneManager : MonoBehaviour {
+
+    [SerializeField]
+    private string gameSceneName = "";
+
+    public void JumpScene(string sceneName)
+    {
+        SceneController.JumpSceneAsync(sceneName);
+    }
+
+    public void JumpGameScene()
+    {
+        SceneController.JumpSceneAsync(gameSceneName);
+    }
+
+}

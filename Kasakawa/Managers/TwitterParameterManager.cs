3338bc7f0c0bb19142505d18a4294e6a21b825f2
nuno <nenowawon@gmail.com>
2019-01-17 10:34:03 +0900

    ツイッターの情報を格納するクラスを作成。

---
+++ Kasakawa/Managers/TwitterParameterManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ツイッターから取得したデータを格納するクラス
+/// </summary>
+public class TwitterParameterManager: SingletonMonoBehaviour<TwitterParameterManager>
+{
+    
+    /// <summary>
+    /// ツイッターアカウントのID
+    /// </summary>
+    private string userID = "";
+
+    private Texture2D iconTexture = null;
+
+    public void SetUserID(string setID)
+    {
+        userID = setID;
+    }
+
+    public void SetUserIcon(Texture2D setTexture)
+    {
+        iconTexture = setTexture;
+    }
+	
+	
+}

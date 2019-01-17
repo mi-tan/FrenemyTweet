9af69614dc7d9ce5754a1da60b90a01cc2d1433c
nuno <nenowawon@gmail.com>
2019-01-17 16:10:56 +0900

    プレイヤーの情報を格納するクラス作成。

---
+++ Kasakawa/Managers/PlayerParameterManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParameterManager : SingletonMonoBehaviour<PlayerParameterManager> {
+
+    [SerializeField]
+    private PlayerParameter playerParameter;
+
+    private int playerHP = 10;
+
+    private int attackPower = 10;
+
+    public int PlayerHP
+    {
+        get
+        {
+            return playerHP;
+        }
+    }
+
+    public int AttackPower
+    {
+        get
+        {
+            return attackPower;
+        }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void CalcStatus()
+    {
+        // HPをランダムに決定する
+        playerHP = UnityEngine.Random.Range(0, 100);
+
+        // 攻撃力をランダムに決定する
+        attackPower = UnityEngine.Random.Range(0, 100);
+    }
+}

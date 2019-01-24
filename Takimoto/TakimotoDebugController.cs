e3d2b7bb60430adb05afa63c504501de77bfbe39
takimoto0501 <takimoto0501@gmail.com>
2019-01-24 11:09:37 +0900

    デバッグ用にRキーでシーン遷移を実装

---
+++ Takimoto/TakimotoDebugController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TakimotoDebugController : MonoBehaviour {
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("TakimotoDebugScene");
+
+            // マウスを表示
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}

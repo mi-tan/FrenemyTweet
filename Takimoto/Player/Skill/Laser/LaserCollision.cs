6f4cbfa5153ad81ea5765eae57635c8d396c29dd
takimoto0501 <takimoto0501@gmail.com>
2019-01-28 11:16:14 +0900

    レーザーの当たり判定を調整

--- Takimoto/Player/Skill/Laser/LaserCollision.cs
+++ Takimoto/Player/Skill/Laser/LaserCollision.cs
@@ -4,29 +4,59 @@ using UnityEngine;
 
 public class LaserCollision : MonoBehaviour
 {
-    private Collider col;
+    private BoxCollider col;
 
     private float time = 0f;
 
-    private float destroyTime = 2.3f;
+    private float startTime = 0.3f;
+    private bool start = false;
 
+    private float largeTime = 0.5f;
+    private float largeSpeed = 5f;
+
+    private float size = 0.1f;
+    private float distance = 27f;
+
+    private float destroyTime = 2.2f;
     private bool destroy = false;
+
+    private float intervalTime = 0.2f;
 
 
     private void Awake()
     {
-        col = GetComponent<Collider>();
+        col = GetComponent<BoxCollider>();
     }
 
     private void Start()
     {
-        StartCoroutine(Collider());
+        col.enabled = false;
+        col.size = new Vector3(size, size, distance);
     }
 
     private void Update()
     {
+        col.size = new Vector3(size, size, distance);
+
         time += Time.deltaTime;
-        if(time >= destroyTime)
+
+        if (time >= startTime)
+        {
+            if (!start)
+            {
+                start = true;
+                StartCoroutine(Collider());
+            }
+
+            if (time < largeTime)
+            {
+                size += Time.deltaTime * largeSpeed;
+            }
+        }
+
+        size = Mathf.Clamp(size, 0.1f, 1f);
+
+        if (time >= destroyTime)
         {
             destroy = true;
         }
@@ -36,7 +66,7 @@ public class LaserCollision : MonoBehaviour
     {
         col.enabled = true;
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(intervalTime);
 
         col.enabled = false;
 

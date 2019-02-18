8f7c75d8ab6d7dc59f2cdfbb05effbf0b967e9be
nuno <nenowawon@gmail.com>
2019-02-18 09:46:56 +0900

    エフェクト削除クラスをPhoton対応。

--- Kasakawa/EditerScripts/AutoObjectDeleter.cs
+++ Kasakawa/EditerScripts/AutoObjectDeleter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 /// <summary>
 /// アタッチされたゲームオブジェクトを開始時に削除
@@ -10,8 +11,19 @@ public sealed class AutoObjectDeleter : MonoBehaviour {
     [SerializeField]
     private float waitTime = 0f;
 
+    [SerializeField]
+    private bool isPhoton = false;
+
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start () {
+
+        if (isPhoton)
+        {
+            //GetComponent<PhotonView>();
+            yield return new WaitForSeconds(waitTime);
+            PhotonNetwork.Destroy(GetComponent<PhotonView>());
+            yield break;
+        }
         Destroy(gameObject, waitTime);
 	}
 }
